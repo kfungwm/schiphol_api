f7b5dfee9030181fe4cb6329d9ed8491e97fc6c2
Kevin Fung <kevinfung87@gmail.com>
2017-08-18 01:21:02 +0200

    added foursqare api

--- index.js
+++ index.js
@@ -18,24 +18,23 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-
 app.get('/flight', function (req, res) {
 // ======= SCHIPHOL Search Flightname details ========
 
 var app_id = "152ae116",
     app_key = "16c9819f9d3ab5e97d46e66d404402ac",
 
-    flightName = req.query.flightNumber,
+    flightName = req.query.flightNumber.toUpperCase().replace(/\s+/g, '');
     // flightName = "CX270",
     url = '/public-flights/flights?app_id=' + app_id + '&app_key=' + app_key + '&flightname=' + flightName;
 
 var options = {
-  "method": "GET",
-  "hostname": "api.schiphol.nl",
-  "port": null,
-  "path": url,
-  "headers": {
-    "resourceversion": "v3"
+  method: "GET",
+  hostname: "api.schiphol.nl",
+  port: null,
+  path: url,
+  headers: {
+    resourceversion: "v3"
   }
 };
 
@@ -56,37 +55,33 @@ function flightDetails() {
         flightData.scheduleTime = parsedData.flights[0].scheduleTime;
         flightData.gate = parsedData.flights[0].gate;
         flightData.terminal = parsedData.flights[0].terminal;
+        flightData.flightIA = parsedData.flights[0].prefixIATA;
 
         flightData.push(flightData);
 
-        matchCity(flightData);
+        matchAirlines(flightData);
     });
   });
 }
-  // flightDetails();
 
-// ====== flightName match the city name =======
+// ====== IATA Match AirlinesName ======
 
+function matchAirlines (flightData) {
 
+  var iata = flightData.flightIA;
+  var airlinesUrl = '/public-flights/airlines/' + iata + '?app_id=' + app_id + '&app_key=' + app_key;
 
-
-function matchCity (flightData) {
-
-  var city_des = flightData.destinations;
-  var cityUrl = '/public-flights/destinations/' + city_des + '?app_id=' + app_id + '&app_key=' + app_key;
-
-  var options2 = {
-    "method": "GET",
-    "hostname": "api.schiphol.nl",
-    "port": null,
-    "path": cityUrl,
-    "headers": {
-      "resourceversion": "v1"
+  var airlines = {
+    method: "GET",
+    hostname: "api.schiphol.nl",
+    port: null,
+    path: airlinesUrl,
+    headers: {
+      resourceversion: "v1"
     }
   };
 
-
-  http.get(options2, function (res) {
+  http.get(airlines, function (res) {
     var chunks = [];
     res.on("data", function (chunk) {
       chunks.push(chunk);
@@ -94,23 +89,59 @@ function matchCity (flightData) {
         var body = Buffer.concat(chunks);
         var parsedData = JSON.parse(body);
 
+        var airlinesData = [];
+        airlinesData.name = parsedData.publicName;
+
+        airlinesData.push(airlinesData);
+
+        matchCity(flightData, airlinesData);
+    });
+  });
+}
+
+
+
+
+// ====== flightName match the city name =======
+
+function matchCity (flightData, airlinesData) {
+
+  var city_des = flightData.destinations;
+  // var iata = s;
+  var cityUrl = '/public-flights/destinations/' + city_des + '?app_id=' + app_id + '&app_key=' + app_key;
+
+  var cityOptions = {
+    method: "GET",
+    hostname: "api.schiphol.nl",
+    port: null,
+    path: cityUrl,
+    headers: {
+      resourceversion: "v1"
+    }
+  };
+
+  http.get(cityOptions, function (res) {
+    var chunks = [];
+    res.on("data", function (chunk) {
+      chunks.push(chunk);
+    }).on("end", function () {
+        var body = Buffer.concat(chunks);
+        var parsedData = JSON.parse(body);
         var cityData = [];
         cityData.city = parsedData.city;
         cityData.country = parsedData.country;
 
         cityData.push(cityData);
 
-        cityWeather(flightData, cityData);
+        cityWeather(flightData, airlinesData, cityData);
     });
   });
 }
-// matchCity();
-
 
 // //========== WEATHER API ===========
 
 
-function cityWeather (flightData, cityData) {
+function cityWeather (flightData, airlinesData, cityData) {
 
   var APPID = 'a335b031bda5798564c74aca0b4a81aa',
       cityName = cityData.city;
@@ -158,28 +189,294 @@ function cityWeather (flightData, cityData) {
         weather.cityName = weather[j].cityName;
         weather.countryName = weather[j].countryName;
 
-     }
-       console.log("-------- laatste api --------");
-       console.log(flightData);
-       console.log(cityData);
-       console.log(weather);
-       // console.log();
-       // console.log();
-       console.log("========== einde api =========");
-       var data = {
-         flightData: flightData, cityData: cityData, weather: weather
-       };
 
-       res.render('flight', data);
+     }
+     foursquareFood(flightData, airlinesData, cityData, weather);
+
     }
   });
 }
-// cityWeather();
+
+// //======= Foursquare Api =======
+
+var CLIENT_ID = 'URUFPNY1Q2C1D4OVRN15VBBP3E3HUT1SOQDQQBPWYLLUTP2Q',
+    CLIENT_SECRET = 'TKPY0CWD3MUCVVAWTSCSGSZBWW3HIRI2LHJGOUXQNUPQJQ4Y';
+
+function foursquareFood(flightData, airlinesData, cityData, weather) {
+  var cityName = cityData.city;
+  var date = flightData.scheduleDate.replace(/[^A-Z0-9]+/ig, "");
+  var section = "food";
+  var limitNumber = 10;
+  foodUrl = 'https://api.foursquare.com/v2/venues/explore?near=' + cityName + '&section=' + section + '&limit=' + limitNumber + '&venuePhotos=1&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=' + date;
+
+  request({url: foodUrl}, function(error, response, body) {
+    if(!error && response.statusCode === 200) {
+      var parsedData = JSON.parse(body);
+      var listData = parsedData.response.groups[0].items;
+
+      var foodList = [];
+
+      for(var i = 0; i < listData.length; i++) {
+        var foodData = {};
+
+        foodData.name = parsedData.response.groups[0].items[i].venue.name;
+        foodData.phone = parsedData.response.groups[0].items[i].venue.contact.phone;
+        foodData.lat = parsedData.response.groups[0].items[i].venue.location.lat;
+        foodData.lng = parsedData.response.groups[0].items[i].venue.location.lng;
+        foodData.address = parsedData.response.groups[0].items[i].venue.location.address;
+        foodData.postalCode = parsedData.response.groups[0].items[i].venue.location.postalCode;
+        foodData.city = parsedData.response.groups[0].items[i].venue.location.city;
+
+        foodData.prefix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].prefix;
+        foodData.suffix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].suffix;
+        foodData.url = parsedData.response.groups[0].items[i].venue.url;
+        foodData.rating = parsedData.response.groups[0].items[i].venue.rating;
+        // console.log(foodData.response.groups[0].items[i].venue.menu.url);
+        // foodData.status = parsedData.response.groups[0].items[i].venue.hours.status;
+
+
+        foodList.push(foodData);
+      }
+      foursquareCoffee(flightData, airlinesData, cityData, weather, foodList);
+
+    }
+  });
+}
+
+function foursquareCoffee(flightData, airlinesData, cityData, weather, foodList) {
+  var cityName = cityData.city;
+  var date = flightData.scheduleDate.replace(/[^A-Z0-9]+/ig, "");
+  var section = "coffee";
+  var limitNumber = 10;
+  coffeeUrl = 'https://api.foursquare.com/v2/venues/explore?near=' + cityName + '&section=' + section + '&limit=' + limitNumber + '&venuePhotos=1&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=' + date;
+
+  request({url: coffeeUrl}, function(error, response, body) {
+    if(!error && response.statusCode === 200) {
+      var parsedData = JSON.parse(body);
+      var listData = parsedData.response.groups[0].items;
+
+      var coffeeList = [];
+
+      for(var i = 0; i < listData.length; i++) {
+        var coffeeData = {};
+
+        coffeeData.name = parsedData.response.groups[0].items[i].venue.name;
+        coffeeData.phone = parsedData.response.groups[0].items[i].venue.contact.phone;
+        coffeeData.lat = parsedData.response.groups[0].items[i].venue.location.lat;
+        coffeeData.lng = parsedData.response.groups[0].items[i].venue.location.lng;
+        coffeeData.address = parsedData.response.groups[0].items[i].venue.location.address;
+        coffeeData.postalCode = parsedData.response.groups[0].items[i].venue.location.postalCode;
+        coffeeData.city = parsedData.response.groups[0].items[i].venue.location.city;
+
+        coffeeData.prefix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].prefix;
+        coffeeData.suffix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].suffix;
+        coffeeData.url = parsedData.response.groups[0].items[i].venue.url;
+        coffeeData.rating = parsedData.response.groups[0].items[i].venue.rating;
+        // console.log(foodData.response.groups[0].items[i].venue.menu.url);
+        // coffeeData.status = parsedData.response.groups[0].items[i].venue.hours.status;
+
+
+        coffeeList.push(coffeeData);
+      }
+      foursquareNightlife(flightData, airlinesData, cityData, weather, foodList, coffeeList);
+
+    }
+  });
+}
+
+
+function foursquareNightlife(flightData, airlinesData, cityData, weather, foodList, coffeeList) {
+  var cityName = cityData.city;
+  var date = flightData.scheduleDate.replace(/[^A-Z0-9]+/ig, "");
+  var section = "nightlife";
+  var limitNumber = 10;
+  nightlifeUrl = 'https://api.foursquare.com/v2/venues/explore?near=' + cityName + '&section=' + section + '&limit=' + limitNumber + '&venuePhotos=1&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=' + date;
+
+  request({url: nightlifeUrl}, function(error, response, body) {
+    if(!error && response.statusCode === 200) {
+      var parsedData = JSON.parse(body);
+      var listData = parsedData.response.groups[0].items;
+
+      var nightlifeList = [];
+
+      for(var i = 0; i < listData.length; i++) {
+        var nightlifeData = {};
+
+        nightlifeData.name = parsedData.response.groups[0].items[i].venue.name;
+        nightlifeData.phone = parsedData.response.groups[0].items[i].venue.contact.phone;
+        nightlifeData.lat = parsedData.response.groups[0].items[i].venue.location.lat;
+        nightlifeData.lng = parsedData.response.groups[0].items[i].venue.location.lng;
+        nightlifeData.address = parsedData.response.groups[0].items[i].venue.location.address;
+        nightlifeData.postalCode = parsedData.response.groups[0].items[i].venue.location.postalCode;
+        nightlifeData.city = parsedData.response.groups[0].items[i].venue.location.city;
+
+        nightlifeData.prefix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].prefix;
+        nightlifeData.suffix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].suffix;
+        nightlifeData.url = parsedData.response.groups[0].items[i].venue.url;
+        nightlifeData.rating = parsedData.response.groups[0].items[i].venue.rating;
+        // console.log(foodData.response.groups[0].items[i].venue.menu.url);
+        // nightlifeData.status = parsedData.response.groups[0].items[i].venue.hours.status;
+
+
+        nightlifeList.push(nightlifeData);
+      }
+      foursquareShops(flightData, airlinesData, cityData, weather, foodList, coffeeList, nightlifeList);
+
+    }
+  });
+}
+
+function foursquareShops(flightData, airlinesData, cityData, weather, foodList, coffeeList, nightlifeList) {
+  var cityName = cityData.city;
+  var date = flightData.scheduleDate.replace(/[^A-Z0-9]+/ig, "");
+  var section = "shops";
+  var limitNumber = 10;
+  shopsUrl = 'https://api.foursquare.com/v2/venues/explore?near=' + cityName + '&section=' + section + '&limit=' + limitNumber + '&venuePhotos=1&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=' + date;
+
+  request({url: shopsUrl}, function(error, response, body) {
+    if(!error && response.statusCode === 200) {
+      var parsedData = JSON.parse(body);
+      var listData = parsedData.response.groups[0].items;
+
+      var shopsList = [];
+
+      for(var i = 0; i < listData.length; i++) {
+        var shopsData = {};
+
+        shopsData.name = parsedData.response.groups[0].items[i].venue.name;
+        shopsData.phone = parsedData.response.groups[0].items[i].venue.contact.phone;
+        shopsData.lat = parsedData.response.groups[0].items[i].venue.location.lat;
+        shopsData.lng = parsedData.response.groups[0].items[i].venue.location.lng;
+        shopsData.address = parsedData.response.groups[0].items[i].venue.location.address;
+        shopsData.postalCode = parsedData.response.groups[0].items[i].venue.location.postalCode;
+        shopsData.city = parsedData.response.groups[0].items[i].venue.location.city;
+
+        shopsData.prefix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].prefix;
+        shopsData.suffix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].suffix;
+        shopsData.url = parsedData.response.groups[0].items[i].venue.url;
+        shopsData.rating = parsedData.response.groups[0].items[i].venue.rating;
+        // console.log(foodData.response.groups[0].items[i].venue.menu.url);
+        // shopsData.status = parsedData.response.groups[0].items[i].venue.hours.status;
+
+
+        shopsList.push(shopsData);
+      }
+      foursquareTrending(flightData, airlinesData, cityData, weather, foodList, coffeeList, nightlifeList, shopsList);
+
+    }
+  });
+}
+
+function foursquareTrending(flightData, airlinesData, cityData, weather, foodList, coffeeList, nightlifeList, shopsList) {
+  var cityName = cityData.city;
+  var date = flightData.scheduleDate.replace(/[^A-Z0-9]+/ig, "");
+  var section = "trending";
+  var limitNumber = 10;
+  trendingUrl = 'https://api.foursquare.com/v2/venues/explore?near=' + cityName + '&section=' + section + '&limit=' + limitNumber + '&venuePhotos=1&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=' + date;
+
+  request({url: trendingUrl}, function(error, response, body) {
+    if(!error && response.statusCode === 200) {
+      var parsedData = JSON.parse(body);
+      var listData = parsedData.response.groups[0].items;
+
+      var trendingList = [];
+
+      for(var i = 0; i < listData.length; i++) {
+        var trendingData = {};
+
+        trendingData.name = parsedData.response.groups[0].items[i].venue.name;
+        trendingData.phone = parsedData.response.groups[0].items[i].venue.contact.phone;
+        trendingData.lat = parsedData.response.groups[0].items[i].venue.location.lat;
+        trendingData.lng = parsedData.response.groups[0].items[i].venue.location.lng;
+        trendingData.address = parsedData.response.groups[0].items[i].venue.location.address;
+        trendingData.postalCode = parsedData.response.groups[0].items[i].venue.location.postalCode;
+        trendingData.city = parsedData.response.groups[0].items[i].venue.location.city;
+
+        trendingData.prefix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].prefix;
+        trendingData.suffix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].suffix;
+        trendingData.url = parsedData.response.groups[0].items[i].venue.url;
+        trendingData.rating = parsedData.response.groups[0].items[i].venue.rating;
+        // console.log(foodData.response.groups[0].items[i].venue.menu.url);
+        // trendingData.status = parsedData.response.groups[0].items[i].venue.hours.status;
+
+
+        trendingList.push(trendingData);
+      }
+      foursquareArts(flightData, airlinesData, cityData, weather, foodList, coffeeList, nightlifeList, shopsList, trendingList);
+
+    }
+  });
+}
+
+
+function foursquareArts(flightData, airlinesData, cityData, weather, foodList, coffeeList, nightlifeList, shopsList, trendingList) {
+
+  var cityName = cityData.city;
+  var date = flightData.scheduleDate.replace(/[^A-Z0-9]+/ig, "");
+  var section = "arts";
+  var limitNumber = 10;
+  artsUrl = 'https://api.foursquare.com/v2/venues/explore?near=' + cityName + '&section=' + section + '&limit=' + limitNumber + '&venuePhotos=1&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=' + date;
+
+
+  request({url: artsUrl}, function(error, response, body) {
+    if(!error && response.statusCode === 200) {
+      var parsedData = JSON.parse(body);
+      var listData = parsedData.response.groups[0].items;
+
+      var artsList = [];
+
+      for(var i = 0; i < listData.length; i++) {
+        var artsData = {};
+
+        artsData.name = parsedData.response.groups[0].items[i].venue.name;
+        artsData.phone = parsedData.response.groups[0].items[i].venue.contact.phone;
+        artsData.lat = parsedData.response.groups[0].items[i].venue.location.lat;
+        artsData.lng = parsedData.response.groups[0].items[i].venue.location.lng;
+        artsData.address = parsedData.response.groups[0].items[i].venue.location.address;
+        artsData.postalCode = parsedData.response.groups[0].items[i].venue.location.postalCode;
+        artsData.city = parsedData.response.groups[0].items[i].venue.location.city;
+
+        artsData.prefix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].prefix;
+        artsData.suffix = parsedData.response.groups[0].items[i].venue.featuredPhotos.items[0].suffix;
+        artsData.url = parsedData.response.groups[0].items[i].venue.url;
+        artsData.rating = parsedData.response.groups[0].items[i].venue.rating;
+
+        artsList.push(artsData);
+      }
+      console.log("-------- All Api Details --------");
+      // console.log(flightData);
+      // console.log(airlinesData);
+      // console.log(cityData);
+      // console.log(weather);
+      console.log(trendingList);
+      console.log("===============");
+      console.log(nightlifeList);
+      console.log("===============");
+      console.log(coffeeList);
+      console.log("===============");
+      console.log(artsList);
+      console.log("===============");
+      console.log(shopsList);
+      console.log("========== End =========");
+
+      var data = {
+        flightData: flightData, airlinesData: airlinesData, cityData: cityData, weather: weather, artsList: artsList, foodList: foodList, coffeeList: coffeeList, nightlifeList: nightlifeList, shopsList: shopsList, trendingList: trendingList
+      };
+
+      res.render('flight', data);
+    }
+  });
+}
+
+
+
+
+
+
 
   flightDetails();
 
 });
-
 
 
 app.listen(3000, () => {
